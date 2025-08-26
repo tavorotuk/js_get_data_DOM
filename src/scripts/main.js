1fd238299762bfc065d056ab275be5765344c42d
Vadym Kirilkov <tav0r0tuk@gmail.com>
2025-08-26 15:09:35 +0200

    finished

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write your code here
+const arrayPopulation = document.querySelectorAll('.population');
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+
+let sum = 0;
+
+for (let el of arrayPopulation) {
+  el = Number(el.textContent.replace(/,/g, ''));
+  sum += el;
+}
+
+const average = sum / arrayPopulation.length;
+
+totalPopulation.textContent = sum.toLocaleString();
+averagePopulation.textContent = Math.round(average).toLocaleString();
