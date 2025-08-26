f9e8d592e2a82ab44c1bc3085883e3d1ffe6b095
Vadym Kirilkov <tav0r0tuk@gmail.com>
2025-08-26 15:32:49 +0200

    make build

---
+++ js_get_data_DOM.f75de5e1.js
@@ -0,0 +1,18 @@
+'use strict';
+const arrayPopulation = document.querySelectorAll('.population');
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+let sum = 0;
+let count = 0;
+for (const el of arrayPopulation){
+    const num = Number(el.textContent.replace(/,/g, ''));
+    if (!isNaN(num)) {
+        sum += num;
+        count++;
+    }
+}
+const average = count > 0 ? sum / count : 0;
+totalPopulation.textContent = sum.toLocaleString();
+averagePopulation.textContent = Math.round(average).toLocaleString();
+
+//# sourceMappingURL=js_get_data_DOM.f75de5e1.js.map
