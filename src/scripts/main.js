efbb0f7cca64440d72040d73e2f20ed69e9ee04d
Vadym Kirilkov <tav0r0tuk@gmail.com>
2025-08-26 15:26:21 +0200

    try to fix problems

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,13 +5,18 @@ const totalPopulation = document.querySelector('.total-population');
 const averagePopulation = document.querySelector('.average-population');
 
 let sum = 0;
+let count = 0;
 
 for (let el of arrayPopulation) {
-  el = Number(el.textContent.replace(/,/g, ''));
-  sum += el;
+  const num = Number(el.textContent.replace(/,/g, ''));
+
+  if (!isNaN(el)) {
+    sum += num;
+    count++;
+  }
 }
 
-const average = sum / arrayPopulation.length;
+const average = count > 0 ? sum / count : 0;
 
 totalPopulation.textContent = sum.toLocaleString();
 averagePopulation.textContent = Math.round(average).toLocaleString();
